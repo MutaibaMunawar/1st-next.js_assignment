b84dd1c6c3d7910a16408793655cd69f731b3b3e
MutaibaMunawar <mutaibachohan8@gmail.com>
2024-10-23 15:05:14 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import cssstyle from './about.module.css'
+
+const About = () => {
+  return (
+    <div className={cssstyle.container}>
+      
+      About
+      <p className={cssstyle.para}>
+        Hi, this is About page
+      </p>
+</div>
+      
+  )
+}
+export default About
---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import cssstyle from './contact.module.css'
+
+const Contact = () => {
+  return (
+    <div className={cssstyle.container} >This is Contact
+    <p className={cssstyle.para}>
+      This is contact page
+
+    </p>
+    </div>
+  )
+}
+
+export default Contact
